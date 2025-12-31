9e9a6972cac666ba9c1b8fccfcfc30daf05e61cb
Kazuki Mitomi <TomichibaGit@gmail.com>
2025-12-31 20:23:10 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -9,21 +9,22 @@ DIR=$HOME
 
 cd "$DIR/ros2_ws"
 
-# ビルド（標準出力・標準エラーを抑制）
+# ビルド
 colcon build > /dev/null 2>&1
 source install/setup.bash
 
 # listener をバックグラウンドで起動
 cd "$DIR/ros2_ws/src/mypkg/mypkg/"
-timeout 10 python3 test_listener.py > /tmp/mypkg_listener.log 2>&1 &
+timeout 10 python3 test_listener.py > /tmp/mypkg_listener.log 2>&1 || true &
+LISTENER_PID=$!
 
 # talker を起動して Publish
-timeout 10 python3 similality_images.py > /tmp/mypkg_talker.log 2>&1
+timeout 10 python3 similality_images.py > /tmp/mypkg_talker.log 2>&1 || true
 
 # listener の終了待ち
-wait
+wait $LISTENER_PID || true
 
-# ログを確認して標準出力に出す
+# ログ内容を確認
 cat /tmp/mypkg_listener.log
 cat /tmp/mypkg_talker.log
 
