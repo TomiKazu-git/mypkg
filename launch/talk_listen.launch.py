8a539a349011433f5450bb5b9d7d50ce4b136151
Kazuki Mitomi <TomichibaGit@gmail.com>
2025-12-31 14:55:20 +0900

    fix

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,19 +1,29 @@
-import launch
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2025 Kazuki Mitomi
+# SPDX-License-Identifier: BSD-3-Clause
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-    talker = launch_ros.actions.Node(
-        package='mypkg',      #パッケージの名前を指定
-        executable='talker',  #実行するファイルの指定
-        )
-    listener = launch_ros.actions.Node(
+    talker = Node(
+        package='mypkg',
+        executable='talker',
+        name='time_publisher',
+        output='screen'
+    )
+
+    listener = Node(
         package='mypkg',
         executable='listener',
-        output='screen'        #ログを端末に出すための設定                                   
-        )
+        name='time_listener',
+        output='screen'
+    )
 
-    return launch.LaunchDescription([talker, listener])
+    return LaunchDescription([
+        talker,
+        listener
+    ])
+
